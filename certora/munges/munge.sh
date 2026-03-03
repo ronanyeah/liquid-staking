6793ecdd23a03f009d279d8de47a85694e8b4957
Kevin Lotz <kevin@certora.com>
2026-03-03 11:23:58 +0100

    chore:

--- certora/munges/munge.sh
+++ certora/munges/munge.sh
@@ -1,3 +1,2 @@
-git apply -3 certora/munges/liquid_staking.patch 
 git apply -3 certora/munges/fees.patch 
 git apply -3 certora/munges/storage.patch 
--- certora/munges/record_patches.sh
+++ certora/munges/record_patches.sh
@@ -1,3 +1,2 @@
-git diff HEAD:contracts/sources/liquid_staking.move  contracts/sources/liquid_staking.move  > certora/munges/liquid_staking.patch;
 git diff HEAD:contracts/sources/fees.move  contracts/sources/fees.move  > certora/munges/fees.patch;
 git diff HEAD:contracts/sources/storage.move  contracts/sources/storage.move  > certora/munges/storage.patch;
